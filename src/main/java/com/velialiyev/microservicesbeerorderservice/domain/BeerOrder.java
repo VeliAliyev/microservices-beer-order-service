6c66379ef545185cc84de6cb235d48f7ba6dc9ec
VeliAliyev <veli_aliyev@outlook.com>
2023-01-02 17:33:09 +0300

    BeerOrder entity added

---
+++ src/main/java/com/velialiyev/microservicesbeerorderservice/domain/BeerOrder.java
@@ -0,0 +1,33 @@
+package com.velialiyev.microservicesbeerorderservice.domain;
+
+
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Setter
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerOrder extends BaseEntity{
+
+    private String customerRef;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToMany(mappedBy = "beerOrder", cascade = CascadeType.ALL)
+    @Fetch(FetchMode.JOIN)
+    private Set<BeerOrderLine> beerOrderLines;
+
+    private OrderStatusEnum orderStatus = OrderStatusEnum.NEW;
+    private String orderStatusCallbackUrl;
+}
