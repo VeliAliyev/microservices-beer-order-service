3f3b778f9be54efc99434d427e33cdf1ac03833f
VeliAliyev <veli_aliyev@outlook.com>
2023-01-02 13:50:09 +0300

    initial commit

---
+++ src/main/java/com/velialiyev/microservicesbeerorderservice/MicroservicesBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.velialiyev.microservicesbeerorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBeerOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesBeerOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velialiyev/microservicesbeerorderservice/MicroservicesBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velialiyev.microservicesbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesBeerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
