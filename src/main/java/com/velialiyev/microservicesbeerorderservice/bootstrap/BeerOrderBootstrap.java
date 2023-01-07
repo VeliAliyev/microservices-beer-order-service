17815052f317cee6ec5b18545a643a35eed7b7c7
VeliAliyev <veli_aliyev@outlook.com>
2023-01-07 17:42:44 +0300

    BeerOrderBootstrap updated

--- src/main/java/com/velialiyev/microservicesbeerorderservice/bootstrap/BeerOrderBootstrap.java
+++ src/main/java/com/velialiyev/microservicesbeerorderservice/bootstrap/BeerOrderBootstrap.java
@@ -13,12 +13,16 @@ import java.util.UUID;
 public class BeerOrderBootstrap implements CommandLineRunner {
 
     private final CustomerRepository customerRepository;
+    public static final String TASTING_ROOM = "Tasting Room";
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
 
     @Override
     public void run(String... args) throws Exception {
         if(customerRepository.count() == 0){
             customerRepository.save(Customer.builder()
-                            .customerName("Tasting Room")
+                            .customerName(TASTING_ROOM)
                             .apiKey(UUID.randomUUID())
                     .build());
         }
